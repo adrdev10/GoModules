308b11d473941d9d6fcd1372e312f09613160a02
adrian silva <adriansilva@adrians-MacBook-Air.local>
2019-04-11 23:31:10 -0400

    Minor upgrade

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package hello
 
 import (
 	"rsc.io/quote"
+	v3 "rsc.io/quote/v3"
 )
 
 // Hello returns a string literal
 func Hello() string {
 	return quote.Hello()
+}
+
+// Proverb returns a string literal
+func Proverb() string {
+	return v3.Concurrency()
+
 }
