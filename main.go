6b300d8cc84f1bda7ab5941f61fab0ec4fa3293a
adrian silva <adriansilva@adrians-MacBook-Air.local>
2019-04-11 23:35:32 -0400

    Clearing unused dependencies

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package hello
 
 import (
-	"rsc.io/quote"
-	v3 "rsc.io/quote/v3"
+	"rsc.io/quote/v3"
 )
 
 // Hello returns a string literal
 func Hello() string {
-	return quote.Hello()
+	return quote.HelloV3()
 }
 
 // Proverb returns a string literal
 func Proverb() string {
-	return v3.Concurrency()
+	return quote.Concurrency()
 
 }
